d4eaffc3c48e02bfafc392246fd8abe52a3aaa19
corey clarke <sombas100@yahoo.com>
2024-05-18 20:28:24 +0100

    Create backend server

---
+++ server/server.js
@@ -0,0 +1,36 @@
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const dotenv = require('dotenv')
+const session = require('express-session')
+const jwt = require('jsonwebtoken')
+
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+app.use(cors())
+
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false, expires: 60000 }
+}))
+
+const uri = process.env.MONGODB_URI;
+mongoose.connect(uri)
+.then(() => {
+    console.log('MongoDB connected')
+})
+.catch((err) => {console.log(err)})
+
+app.listen(PORT, () => {console.log(`Server is listening on port: ${PORT}`)})
+
+app.get('/test',(req, res) => {
+    console.log('API is running!')
+    res.send('WORKING').json()
+})

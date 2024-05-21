6d05b47144a378454e4afeb5d6066c49f4aca223
corey clarke <sombas100@yahoo.com>
2024-05-21 17:52:14 +0100

    Create login/register logic and handle token middleware

---
+++ server/controllers/UserController.js
@@ -0,0 +1,60 @@
+const express = require('express')
+const router = express.Router()
+const { generateToken, verifyToken} = require('../middleware/authMiddleware')
+
+const bcrypt = require('bcryptjs')
+const { User } = require('../models/User')
+
+const register = async (req, res) => {
+    const { email, password } = req.body
+
+    try {
+        let user = await User.findOne({ email })
+
+        if (user) {
+            return res.status(400).json({ error: 'User already exists'})
+        }
+
+    const hashedPassword = bcrypt.hashSync(password, 10)
+
+    user = new User({
+        email,
+        password: hashedPassword
+    })
+
+    await user.save()
+    const token = generateToken(user)
+
+    res.json({ token })
+    } catch (error) {
+        console.error(error)
+        res.status(500).send('Internal Server Error')
+    }
+}
+
+const login = async (req, res) => {
+    const { email, password } = req.body
+
+    try {
+        let user = await User.findOne({ email })
+        if (!user) {
+            return res.status(400).json({ message: 'Invalid credentials'})
+        }
+    const isMatch = await bcrypt.compare(password, user.password)
+    if (!isMatch) {
+        return res.status(400).json({message: 'Invalid Credentials' })
+    }
+
+    const token = generateToken(user)
+
+    res.json({ token })
+    } catch (error) {
+        console.error(error)
+        res.status(500).send('Internal server Error')
+    }
+}
+
+module.exports = {
+    register,
+    login
+}

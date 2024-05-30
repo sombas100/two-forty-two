b187504897f0cc43e798969fbb7d4607b2e6a912
corey clarke <sombas100@yahoo.com>
2024-05-30 23:21:05 +0100

    changes

--- server/server.js
+++ server/server.js
@@ -16,12 +16,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.use(cors())
 
-app.use(session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: true,
-    cookie: { secure: false, expires: 60000 }
-}))
+
 
 const uri = process.env.MONGODB_URI;
 mongoose.connect(uri)
